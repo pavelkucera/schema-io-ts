f3ce7ebc50b8cf67b9386078f1680ba83a834069
Pavel Kučera <pave.kucera@gmail.com>
2020-04-13 18:47:54 +0200

    Split shchema type definitions on multiple lines

--- src/Schema.ts
+++ src/Schema.ts
@@ -11,32 +11,42 @@ type NumberRange =
   & ({ minimum?: number } | { exclusiveMinimum?: number })
   & ({ maximum?: number } | { exclusiveMaximum?: number })
 
-export type NumberSchema = CommonSchema & NumberRange & {
-  type: 'number'
-  format?: 'double' | 'float' | string
-}
+export type NumberSchema =
+  & CommonSchema
+  & NumberRange
+  & {
+    type: 'number'
+    format?: 'double' | 'float' | string
+  }
 
 // todo: BigInt support
-export type IntegerSchema = CommonSchema & NumberRange & {
-  type: 'integer'
-  format?: 'int32' | 'int64' | string
-}
+export type IntegerSchema =
+  & CommonSchema
+  & NumberRange
+  & {
+    type: 'integer'
+    format?: 'int32' | 'int64' | string
+  }
 
 export type NumericSchema =
   | NumberSchema
   | IntegerSchema
 
-export type StringSchema = CommonSchema & {
-  type: 'string'
-  minLength?: number
-  maxLength?: number
-  format?: 'date' | 'date-time' | 'password' | 'byte' | 'binary' | string
-  pattern?: string
-}
+export type StringSchema =
+  & CommonSchema
+  & {
+    type: 'string'
+    minLength?: number
+    maxLength?: number
+    format?: 'date' | 'date-time' | 'password' | 'byte' | 'binary' | string
+    pattern?: string
+  }
 
-export type BooleanSchema = CommonSchema & {
-  type: 'boolean'
-}
+export type BooleanSchema =
+  & CommonSchema
+  & {
+    type: 'boolean'
+  }
 
 export type Schema<T> =
   T extends number ? NumericSchema :
