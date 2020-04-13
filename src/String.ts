2c2c3241680463cd4a196c5a3ba8e892614bd761
Pavel Kučera <pave.kucera@gmail.com>
2020-04-13 19:14:35 +0200

    Declare StringSchemaType

---
+++ src/String.ts
@@ -0,0 +1,18 @@
+import { SchemaType } from '@lib/SchemaType'
+import { CommonSchema, StringSchema } from '@lib/Schema'
+import * as t from 'io-ts'
+
+export class StringSchemaType extends SchemaType<StringSchema, string> {
+  constructor(schema: CommonSchema) {
+    super(
+      t.string,
+      {
+        type: 'string',
+        ...schema
+      },
+    );
+  }
+}
+
+export const string = (schema: CommonSchema) =>
+  new StringSchemaType(schema)
