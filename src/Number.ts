0e611da2ffc93360e684805c5c473dbf53797be5
Pavel Kučera <pave.kucera@gmail.com>
2020-05-17 13:50:03 +0200

    Define NumberSchemaType

---
+++ src/Number.ts
@@ -0,0 +1,18 @@
+import { SchemaType } from '@lib/SchemaType'
+import { CommonSchema, NumberSchema } from '@lib/Schema'
+import * as t from 'io-ts'
+
+export class NumberSchemaType extends SchemaType<NumberSchema, t.NumberC> {
+  constructor(schema: CommonSchema) {
+    super(
+      {
+        type: 'number',
+        ...schema
+      },
+      t.number
+    )
+  }
+}
+
+export const number = (schema: CommonSchema) =>
+  new NumberSchemaType(schema)
