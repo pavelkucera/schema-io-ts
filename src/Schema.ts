d9095f09480ad7124c268150b216d8405749c215
Pavel Kučera <pave.kucera@gmail.com>
2020-05-17 13:35:28 +0200

    Replace Schema types by interfaces

--- src/Schema.ts
+++ src/Schema.ts
@@ -3,55 +3,42 @@ export type CommonSchema = {
 }
 
 // todo: BigInt support
-// TypeScript does not do an excess property check on non-discriminated unions so
-// the construct has no practical effect
-// https://github.com/microsoft/TypeScript/issues/20863
-type NumberRange =
-  & ({ minimum?: number } | { exclusiveMinimum?: number })
-  & ({ maximum?: number } | { exclusiveMaximum?: number })
+interface NumberRange {
+  minimum?: number
+  exclusiveMinimum?: number
+  maximum?: number
+  exclusiveMaximum?: number
+}
 
-export type NumberSchema =
-  & CommonSchema
-  & NumberRange
-  & {
-    type: 'number'
-    format?: 'double' | 'float' | string
-  }
+export interface NumberSchema extends CommonSchema, NumberRange {
+  type: 'number'
+  format?: 'double' | 'float' | string
+}
 
-// todo: BigInt support
-export type IntegerSchema =
-  & CommonSchema
-  & NumberRange
-  & {
-    type: 'integer'
-    format?: 'int32' | 'int64' | string
-  }
+export interface IntegerSchema extends CommonSchema, NumberRange {
+  type: 'integer'
+  format?: 'int32' | 'int64' | string
+}
 
 export type NumericSchema =
   | NumberSchema
   | IntegerSchema
 
-export type StringSchema =
-  & CommonSchema
-  & {
-    type: 'string'
-    minLength?: number
-    maxLength?: number
-    format?: 'date' | 'date-time' | 'password' | 'byte' | 'binary' | string
-    pattern?: string
-  }
+export interface StringSchema extends CommonSchema {
+  type: 'string'
+  minLength?: number
+  maxLength?: number
+  format?: 'date' | 'date-time' | 'password' | 'byte' | 'binary' | string
+  pattern?: string
+}
 
-export type BooleanSchema =
-  & CommonSchema
-  & {
-    type: 'boolean'
-  }
+export interface BooleanSchema extends CommonSchema {
+  type: 'boolean'
+}
 
-export type UnionSchema<CS extends Array<any>> =
-  & CommonSchema
-  & {
-    oneOf: Array<Schema<CS[number]>>
-  }
+export interface UnionSchema<CS extends Array<any>> extends CommonSchema {
+  oneOf: Array<Schema<CS[number]>>
+}
 
 export type Schema<T> =
   T extends number ? NumericSchema :
